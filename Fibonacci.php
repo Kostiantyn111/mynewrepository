cb02c9363cc2f0b7b24686e0aa1bdf5f66e72c30
Kostiantyn111 <127661718+Kostiantyn111@users.noreply.github.com>
2024-01-06 15:12:21 +0200

    Update Fibonacci.php

--- Fibonacci.php
+++ Fibonacci.php
@@ -1,23 +1,25 @@
 <?php
-function fibonacciSum($n)
-{
-    $a = 0;
-    $b = 1;
-    $sum = 0;
 
-    for ($i = 1; $i <= $n; $i++) {
-        $c = $a + $b;
-        $sum += $c;
+function fibonacci($n) {
+    $fib_sequence = [0, 1];
 
-        $a = $b;
-        $b = $c;
+    for ($i = 2; $i < $n; $i++) {
+        $fib_sequence[] = $fib_sequence[$i - 1] + $fib_sequence[$i - 2];
     }
 
-    return $sum;
+    return $fib_sequence;
 }
 
+function sum_of_fibonacci($n) {
+    return array_sum(fibonacci($n));
+}
 
-$numbers = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578];
-$totalSum = fibonacciSum(count($numbers));
+// Приклад використання:
+$n = 10;
+$result = fibonacci($n);
+$sum = sum_of_fibonacci($n);
 
-echo "Сума чисел Фібоначчі: $totalSum";
+echo "Послідовність Фібоначчі для $n чисел: " . implode(', ', $result) . PHP_EOL;
+echo "Сума чисел Фібоначчі: $sum" . PHP_EOL;
+
+?>
